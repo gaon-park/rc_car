da5ec7e25957911e1e49e1dfe556bb1e388fd489
gaon-park <ondol1224@gmail.com>
2023-11-20 16:03:10 +0900

    [fix] multithread programming

--- pi/mpu6050/esp.ino
+++ pi/mpu6050/esp.ino
@@ -4,53 +4,134 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include "EspMQTTClient.h"
+#include "Thread.h"
+#include "ThreadController.h"
 
 Adafruit_MPU6050 mpu;
 
 EspMQTTClient client(
-  "MULTI_GUEST",   //wifi SSID
-  "guest1357",     //wifi password
+  "MULTI_GUEST",   // wifi SSID
+  "guest1357",     // wifi password
   "70.12.230.98",  // MQTT Broker server ip
   "MQTTUsername",
   "MQTTPassword",
   "TestClient",
   1883);
 
-const int go_button = 18;    // the number of the pushbutton pin
-const int back_button = 19;  // the number of the LED pin
-volatile unsigned long button_time = 0, last_button_time = 0;
-volatile bool go_button_pressed = false, back_button_pressed = false;
+const int go_button = 18;     // the number of the go button pin
+const int back_button = 19;   // the number of the back button pin
+const int buzzer_button = 3;  // the number of the buzzer button pin
+
+// 한 번만 보내기 위한 flg 변수
+volatile bool command_go = false,
+              command_back = false,
+              command_stop = false,
+              command_left = false,
+              command_right = false,
+              command_mid = false;
+volatile bool command_buzzer = false;
+
+char *cmd_topic = "command";
+char *etc_topic = "etc";
 
 //송신용 tx()
-char *tx_topic = "command";
-void tx(char *cmd) {
-  client.publish(tx_topic, cmd);  //topic , cmd
+void tx(char *topic, char *cmd) {
+  client.publish(topic, cmd);  //topic , cmd
 }
 
-void go_changed(void) {
-  button_time = millis();
-  if (button_time - last_button_time > 100) {
-    go_button_pressed = !go_button_pressed;
-    last_button_time = button_time;
+// mpu Thread
+class MPUThread : public Thread {
+public:
+  sensors_event_t a, g, temp;
+  void cmd_mpu_check(sensors_vec_t ac) {
+    if (ac.y >= 5 && !command_left) {
+      // Serial.println("left");
+      tx(cmd_topic, "left");
+      command_left = true;
+    } else if (ac.y <= -6 && !command_right) {
+      // Serial.println("right");
+      tx(cmd_topic, "right");
+      command_left = false;
+      command_right = true;
+      command_mid = false;
+    } else if (-6 < ac.y && ac.y < 5 && !command_mid) {
+      tx(cmd_topic, "mid");
+      command_left = false;
+      command_right = false;
+      command_mid = true;
+    }
+  }
+
+  void run() {
+    mpu.getEvent(&a, &g, &temp);
+    cmd_mpu_check(a.acceleration);
+    runned();
+  }
+};
+
+void cmd_button_check(void) {
+  if (digitalRead(go_button) == LOW && !command_go) {
+    // Serial.println("go");
+    tx(cmd_topic, "go");
+    command_go = true;
+    command_back = false;
+    command_stop = false;
+  } else if (digitalRead(back_button) == LOW && !command_back) {
+    // Serial.println("back");
+    tx(cmd_topic, "back");
+    command_go = false;
+    command_back = true;
+    command_stop = false;
+  } else if (digitalRead(go_button) == HIGH && digitalRead(back_button) == HIGH && !command_stop) {
+    // Serial.println("stop");
+    tx(cmd_topic, "stop");
+    command_go = false;
+    command_back = false;
+    command_stop = true;
   }
 }
 
-void back_changed(void) {
-  button_time = millis();
-  if (button_time - last_button_time > 100) {
-    back_button_pressed = !back_button_pressed;
-    last_button_time = button_time;
+void etc_button_check(void) {
+  if (digitalRead(buzzer_button) == LOW && !command_buzzer) {
+    // Serial.println("buzzer_on");
+    tx(etc_topic, "buzzer_on");
+    command_buzzer = true;
+  } else if (digitalRead(buzzer_button) == HIGH && command_buzzer) {
+    // Serial.println("buzzer_off");
+    tx(etc_topic, "buzzer_off");
+    command_buzzer = false;
   }
+}
+
+// create thread
+Thread cmd_th = Thread(),
+       etc_th = Thread();
+MPUThread mpu_th = MPUThread();
+ThreadController controller = ThreadController();
+
+// This is the callback for the Timer
+void timerCallback() {
+  controller.run();
 }
 
 void setup(void) {
   Serial.begin(115200);
   client.enableHTTPWebUpdater();
   client.enableOTA();
+
   pinMode(go_button, INPUT_PULLUP);
   pinMode(back_button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(go_button), go_changed, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(back_button), back_changed, CHANGE);
+  pinMode(buzzer_button, INPUT_PULLUP);
+
+  // callback thread func
+  cmd_th.onRun(cmd_button_check);
+  cmd_th.setInterval(50);
+  etc_th.onRun(etc_button_check);
+  etc_th.setInterval(50);
+
+  controller.add(&mpu_th);
+  controller.add(&cmd_th);
+  controller.add(&etc_th);
 
   while (!Serial)
     delay(10);  // will pause Zero, Leonardo, etc until serial console opens
@@ -70,43 +151,11 @@ void setup(void) {
   delay(100);
 }
 
-void read_command(sensors_vec_t ac) {
-  if (go_button_pressed) {
-    tx("go");
-  } else if (back_button_pressed) {
-    tx("back");
-  } else {
-    tx("stop");
-  }
-
-  if (ac.y >= 6) {
-    // Serial.println("left");
-    tx("left");
-  } else if (ac.y <= -6) {
-    // Serial.println("right");
-    tx("right");
-  } else {
-    tx("mid");
-  }
-}
-
 void onConnectionEstablished() {
   //client.loop() 에 의해 호출되는 API
 }
 
 void loop() {
-
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-  /* Print out the values */
-  // Serial.print("Acceleration X: ");
-  // Serial.print(a.acceleration.x);
-  // Serial.print(", Y: ");
-  // Serial.print(a.acceleration.y);
-  // Serial.println();
-  read_command(a.acceleration);
+  controller.run();
   client.loop();
-  delay(500);
 }
