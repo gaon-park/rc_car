4697ad6b2bb116f8ac9761d52ce3f53558f97346
gaon-park <ondol1224@gmail.com>
2023-11-21 16:10:43 +0900

    [fix] camera streaming

--- pi/app.py
+++ pi/app.py
@@ -6,12 +6,89 @@ import paho.mqtt.client as mqtt
 import socket
 from sense_hat import SenseHat
 from gpiozero import TonalBuzzer
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
+
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+import libcamera
 
 isFront = False
 isBack = False
 isLeft = False
 isRight = False
 cmdCnt = 0
+PAGE = """\
+<html>
+<head>
+<title>picamera2 MJPEG streaming demo</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        global output
+        global PAGE
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
 class SenseHatThread(QThread):
     def __init__(self):
@@ -220,23 +297,23 @@ class CmdThread(QThread):
 
 class CameraThread(QThread):
     def __init__(self):
+        global output
         super().__init__()
-        self.camera = cv2.VideoCapture(-1)
-        self.camera.set(3, 640)
-        self.camera.set(4, 480)
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_video_configuration(main={"size": (640, 480)}))
+        self.picam2.configure(self.picam2.create_video_configuration(transform=libcamera.Transform(hflip=1, vflip=1)))
 
+        self.picam2.start_recording(JpegEncoder(), FileOutput(output))
     def run(self):
-        while self.camera.isOpened():
-            _, image = self.camera.read()
-            image = cv2.flip(image, -1)
-            cv2.imshow('camera test', image)
-
-            if cv2.waitKey(1) == ord('q'):
-                break
-
-        cv2.destoryAllWindows()
+        try:
+            address = ('', 8000)
+            server = StreamingServer(address, StreamingHandler)
+            server.serve_forever()
+        finally:
+            self.picam2.stop_recording()
 
 
+output = StreamingOutput()
 if __name__ == '__main__':
     cameraTh = CameraThread()
     cameraTh.start()
