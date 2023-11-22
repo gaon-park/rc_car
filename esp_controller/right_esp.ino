9e144c087ff546c2865c23a2b9f578e7b9b08c5a
gaon-park <ondol1224@gmail.com>
2023-11-22 10:37:20 +0900

    [add] keypad

--- esp_controller/right_esp.ino
+++ esp_controller/right_esp.ino
@@ -6,6 +6,7 @@
 #include "ThreadController.h"
 #include <Arduino.h>
 #include <RotaryEncoder.h>
+#include <Keypad.h>
 
 EspMQTTClient client(
   "MULTI_GUEST",   // wifi SSID
@@ -19,6 +20,22 @@ EspMQTTClient client(
 const int rotary_encoder_sw = 36;
 const int rotary_encoder_data = 39;
 const int rotary_encoder_clk = 34;
+
+const byte ROWS = 4;  // define row 4
+const byte COLS = 4;  // define column 4
+const char keys[ROWS][COLS] = {
+  { '0', '1', '2', '3' },
+  { '4', '5', '6', '7' },
+  { '8', '9', 'A', 'B' },
+  { 'C', 'D', 'E', 'F' }
+};
+
+// connect row ports of the button to corresponding IO ports on the board
+byte row_pins[ROWS] = { 18, 5, 17, 16 };
+// connect column ports of the button to corresponding IO ports on the board
+byte col_pins[COLS] = { 23, 22, 21, 19 };
+// call class library performance function of Keypad
+Keypad keypad = Keypad(makeKeymap(keys), row_pins, col_pins, ROWS, COLS);
 
 volatile int counter = 0,  // 회전 카운터 측정용
   currentStateCLK = 0,     // CLK의 현재 신호상태 저장용
@@ -62,8 +79,72 @@ void rotary_check(void) {
   lastEncoded = encoded;
 }
 
+void keypad_check(void) {
+  char key = keypad.getKey();
+  if (key != NO_KEY) {
+    String data = "";
+    switch (key) {
+      case '0':
+        data = "출근";
+        break;
+      case '1':
+        data = "퇴근";
+        break;
+      case '2':
+        data = "취업";
+        break;
+      case '3':
+        data = "연애";
+        break;
+      case '4':
+        data = "좋다";
+        break;
+      case '5':
+        data = "싫다";
+        break;
+      case '6':
+        data = "!";
+        break;
+      case '7':
+        data = "?";
+        break;
+      case '8':
+        data = "따라와";
+        break;
+      case '9':
+        data = "저리가";
+        break;
+      case 'A':
+        data = "비켜";
+        break;
+      case 'B':
+        data = "안녕";
+        break;
+      case 'C':
+        data = "^~^";
+        break;
+      case 'D':
+        data = "ㅇ0ㅇ";
+        break;
+      case 'E':
+        data = "ㅠ_ㅜ";
+        break;
+      case 'F':
+        data = "ㅡ_ㅡ";
+        break;
+    }
+    char tx_data[10] = {};
+    for (int i = 0; i < data.length(); i++) {
+      tx_data[i] = data[i];
+    }
+    // Serial.println(tx_data);
+    tx(etc_topic, tx_data);
+  }
+}
+
 // create thread
-Thread rotary_th = Thread();
+Thread rotary_th = Thread(),
+       keypad_th = Thread();
 ThreadController controller = ThreadController();
 
 // This is the callback for the Timer
@@ -84,8 +165,11 @@ void setup(void) {
   // callback thread func
   rotary_th.onRun(rotary_check);
   rotary_th.setInterval(10);
+  keypad_th.onRun(keypad_check);
+  keypad_th.setInterval(10);
 
   controller.add(&rotary_th);
+  controller.add(&keypad_th);
 
   while (!Serial)
     delay(10);  // will pause Zero, Leonardo, etc until serial console opens
